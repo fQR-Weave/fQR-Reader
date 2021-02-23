62d9f785b7813561c238fe8cb600cde123eb50a4
wojak-frog <37157916+wojak-frog@users.noreply.github.com>
2021-02-23 22:24:30 +0200

    Update and rename reader.js to bundle.js

--- bundle.js
+++ bundle.js
@@ -1,18 +1,19 @@
 const Qrcode = window.qrcode;
-
+const arweave = Arweave.init();
 const video = document.createElement("video");
 const canvasElement = document.getElementById("qr-canvas");
 const canvas = canvasElement.getContext("2d");
 const qrResult = document.getElementById("qr-result");
 const outputData = document.getElementById("outputData");
 const btnScanQR = document.getElementById("btn-scan-qr");
-const fqr = document.getElementById('fqr');
 
 let scanning = false;
 
 qrcode.callback = res => {
   if (res) {
     outputData.innerText = res;
+    outputData.innerText = `${res.slice(0,5)}...${res.slice(-5, -1)}`;
+    window.item_id = res;
     scanning = false;
 
     video.srcObject.getTracks().forEach(track => {
@@ -55,4 +56,28 @@ function scan() {
   } catch (e) {
     setTimeout(scan, 300);
   }
+}
+
+async function readFqr() {
+  let detected_fqr = window.item_id;
+  console.log(111, detected_fqr)
+  let verifiedWalletsArray = [];
+  const verification_address = 'MtgIRVxVRaooHlL3vHE4Bu875vtnDelgJzwrZ7WnDyo';
+  const res = await arweave.wallets.getLastTransactionID(verification_address);
+  let wallets = await arweave.transactions.getData(res, {decode:true, string:true})
+  // array of verified generators addresses
+  verifiedWalletsArray = wallets.split(' ');
+
+  try {
+    const tx = await arweave.transactions.get(detected_fqr)
+    console.log(tx);
+    if ( (verifiedWalletsArray.find(owner => owner === tx['owner'])) ) {
+              window.open(`https://arweave.net/${detected_fqr}`)
+            } else {alert('invalid transaction ID')}
+            
+  } catch(err) {
+    alert("FQR not detected")
+  }
+  
+
 }
